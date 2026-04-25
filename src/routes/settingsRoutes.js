878d5ddd334ee0f3b5eed092f2b7495529ca0afd
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-04-26 01:17:51 +0600

    fix error on settingsRoute.js

--- src/routes/settingsRoutes.js
+++ src/routes/settingsRoutes.js
@@ -5,17 +5,14 @@ import { uploadFields } from "../middleware/upload.js";
 
 const router = Router();
 
-// পাবলিক রাউট
+
 router.get("/", getSettings);
 
-// প্রোটেক্টেড রাউটস (শুধু অ্যাডমিন)
+
 router.put(
   "/",
   verifyToken,
   (req, res, next) => {
-    // ডিবাগ লগ
-    console.log("🔧 Settings update request received");
-    console.log("Content-Type:", req.headers['content-type']);
     next();
   },
   uploadFields('settings', [
@@ -24,7 +21,7 @@ router.put(
     { name: 'ogImage', maxCount: 1 }
   ]),
   (req, res, next) => {
-    console.log("📁 Files received:", req.files ? Object.keys(req.files) : "No files");
+
     next();
   },
   updateSettings
